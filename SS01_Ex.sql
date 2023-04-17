8c55dc8ae2c3e4e41cf4da9e1cf2f1f6b7ae282d
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-04-17 14:08:20 +0700

    1704

---
+++ SS01_Ex.sql
@@ -0,0 +1,8 @@
+create table Class (
+id int auto_increment key,
+name varchar(255));
+create table Teacher (
+id int auto_increment key,
+name varchar(255),
+age int,
+country varchar(255));
--- SS01_SimpleMySQL.sql
+++
@@ -1,4 +0,0 @@
-SELECT * FROM jv10_demo.class;
-update class set nameClass="jv15" where idClass = 3;
-select nameClass, idClass from class;
-insert into class values (null,"jv20",25);
